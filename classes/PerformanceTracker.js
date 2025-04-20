a16af91bf53ec3b677c78ec29b4fe2044f4c7e85
Tidalu <127156318+tidalu@users.noreply.github.com>
2025-04-20 13:34:15 +0200

    Add PerformanceTracker to log snf traack shoe wear

---
+++ classes/PerformanceTracker.js
@@ -0,0 +1,46 @@
+class PerformanceTracker {
+    constructor() {
+        this.logs= [];
+    }
+
+
+    logStep(athlete, shoe, stepNumber) {
+        this.logs.push([
+            athleteName = athlete.name,
+            shoeType = shoe.constructor.name,
+            shoeModel = shoe.modelName,
+            terrain = athlete.preferredTerrain,
+            activityLevel = athlete.activityLevel,
+            stepNumber, 
+            wearLevel = shoe.wearLevel.toFixed(2),
+            comfortScore = shoe.getComfortScore().toFixed(2),
+        ])
+    }
+
+
+    simulateAndTrack(athlete, shoe, steps = 10) {
+        for(let i = 0; i < steps; i ++) {
+            athlete.simulateActivity(shoe, 1);
+            this.logStep(athlete, shoe, i + 1);
+        }
+    }
+
+    getLogs() {
+        return this.logs
+    }
+
+    printLogs() {
+        console.table(this.logs, [
+            'athleteName',
+            'shoeType',
+            'shoeModel',
+            'terrain',
+            'activityLevel',
+            'stepNumber',
+            'wearLevel',
+            'comfortScore'
+        ])
+    }
+}
+
+module.exports = PerformanceTracker;
