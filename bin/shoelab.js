f637fb0174351a5c6d50a23badec8ca8be34cc9a
Tidalu <127156318+tidalu@users.noreply.github.com>
2025-04-22 00:57:42 +0200

    adedd interactiveness

--- bin/shoelab.js
+++ bin/shoelab.js
@@ -2,6 +2,8 @@
 
 const { Command } = require("commander");
 const chalk = require("chalk");
+const ora = require("ora");
+const chalkAnimation = require("chalk-animation");
 const path = require("path");
 const dataPath = path.join(__dirname, "../data");
 const ShoeFactory = require("../classes/ShoeFactory");
@@ -11,15 +13,15 @@ const RecommendationEngine = require("../classes/RecommendationEngine");
 const { saveJSON, loadJSON } = require("../utils/FileManager");
 const { askQuestion } = require("../utils/askQuestion");
 const loadExistingFiles = require("../utils/loadExistingUserData");
-const {loadShoeData, saveShoeData} = require("../utils/LoadShoeData");
+const { loadShoeData, saveShoeData } = require("../utils/LoadShoeData");
 const reviveShoes = require("../utils/reviveShoes");
 const {
-    getActiveUserProfile,
-    getActiveUserShoes,
-    getActiveUserSelectedShoe,
-    activeUser
+  getActiveUserProfile,
+  getActiveUserShoes,
+  getActiveUserSelectedShoe,
+  activeUser,
 } = require("../utils/getActiveUser");
-const  visualizeWearLevel = require("../utils/visualizeWearLevel");
+const visualizeWearLevel = require("../utils/visualizeWearLevel");
 
 const program = new Command();
 let shoeData = loadShoeData();
@@ -32,7 +34,11 @@ program
   .command("init")
   .description("Create a new user profile")
   .action(async () => {
-    console.log(chalk.blue("👟 Welcome to ShoeLab CLI!"));
+    const animation = chalkAnimation.rainbow(
+      "👟 Welcome to ShoeLab CLI! Initializing..."
+    );
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    animation.stop();
 
     profile = await loadExistingFiles();
     if (!profile) {
@@ -67,13 +73,12 @@ program
   .command("generate")
   .description("Generate shoes")
   .action(async () => {
-    console.log("\n🔧 Generating shoes...");
-
+    const spinner = ora("🔧 Generating shoes...").start();
+    await new Promise((r) => setTimeout(r, 1000));
     shoes = ShoeFactory.generateMany("RunningShoe", 5).concat(
       ShoeFactory.generateMany("HikingBoot", 5)
     );
-
-    console.log(chalk.green("Shoes generated successfully!"));
+    spinner.succeed("🎉 Shoes generated successfully!");
 
     const showShoeList = await askQuestion(
       "Would you like to see the generated shoes? (yes/no) "
@@ -106,7 +111,10 @@ program
   .command("recommend")
   .description("Recommend shoes")
   .action(async () => {
-    console.log("\n📊 Recommending shoes...");
+    const recommendSpinner = ora("📊 Analyzing profile and finding the best matches...").start();
+    await new Promise(r => setTimeout(r, 1500));
+    recommendSpinner.succeed("🔍 Recommendations ready!");
+    
 
     const active = activeUser();
     finalName = active.name;
@@ -117,7 +125,10 @@ program
     // load user shoes
     const savedShoes = getActiveUserShoes();
 
-    const ranked = RecommendationEngine.recommend(profile, reviveShoes(savedShoes));
+    const ranked = RecommendationEngine.recommend(
+      profile,
+      reviveShoes(savedShoes)
+    );
     ranked.slice(0, 3).forEach((r, i) => {
       console.log(`\n#${i + 1} 🥇 Score: ${r.score}`);
       console.table(r.shoe);
@@ -140,8 +151,8 @@ program
     console.log(
       `\n👟 You selected: ${selectedShoe.brand} ${selectedShoe.modelName}`
     );
-    if(!shoeData) {
-        shoeData = loadShoeData();
+    if (!shoeData) {
+      shoeData = loadShoeData();
     }
     if (!shoeData[selectedShoe.modelName]) {
       shoeData[selectedShoe.modelName] = {
@@ -154,10 +165,7 @@ program
     process.exit(0);
   });
 
-program
-  .command("run")
-  .description("Log a run")
-  .action(runAction);
+program.command("run").description("Log a run").action(runAction);
 
 program
   .command("delete")
@@ -172,8 +180,7 @@ program
       if (allModels.length === 0) {
         console.log("No shoes to delete.");
       } else {
-        
-        console.log("\n📂 Shoes available for deletion:", allModels);
+        console.log("\n📂 Shoes available for deletion:");
         allModels.forEach((model, i) => {
           console.log(`${i + 1}. ${model}`);
         });
@@ -186,16 +193,14 @@ program
         if (!isNaN(index) && allModels[index - 1]) {
           const deletedModel = allModels[index - 1];
           delete shoeData[deletedModel];
-          console.log(`🗑️ Deleted shoe: ${deletedModel}`);
+          ora().succeed(`🗑️ Deleted shoe: ${chalk.red.bold(deletedModel)}`);
+
           saveShoeData(shoeData);
         } else {
           console.log("❌ Invalid selection. No shoe deleted.");
         }
-
       }
-
     }
-    
 
     process.exit(0);
   });
@@ -214,108 +219,106 @@ program
 
 program.parse(process.argv);
 
+async function runAction() {
+  const animation = chalkAnimation.neon("✅ Run logged successfully!")
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  animation.stop();
+  const distanceRan = parseFloat(
+    await askQuestion("🏃‍♂️ How many kilometers did you run?    ")
+  );
+  let selectedShoe = reviveShoes(getActiveUserSelectedShoe());
 
-async function runAction ()  {
-    console.log(chalk.green("Run logged successfully!"));
-    const distanceRan = parseFloat(
-      await askQuestion("🏃‍♂️ How many kilometers did you run? ")
+  if (distanceRan > 0) {
+    if (!shoeData) {
+      shoeData = loadShoeData();
+    }
+
+    if (!shoeData[selectedShoe.modelName]) {
+      shoeData[selectedShoe.modelName] = {
+        totalDistance: 0,
+        durabilityLeft: selectedShoe.durabilityLeft,
+        wearLevel: 0,
+      };
+    }
+
+    shoeData[selectedShoe.modelName].totalDistance += distanceRan;
+    shoeData[selectedShoe.modelName].durabilityLeft -= distanceRan;
+
+    const wearLevel = PerformanceTracker.calculateWearLevel(
+      shoeData[selectedShoe.modelName].totalDistance,
+      selectedShoe.durabilityLeft
     );
-    let selectedShoe = reviveShoes(getActiveUserSelectedShoe());
+    shoeData[selectedShoe.modelName].wearLevel = wearLevel.toFixed(2);
 
-    
-    if (distanceRan > 0) {
-      if(!shoeData) {
-        shoeData = loadShoeData();
-      }
+    // updateed shoe status
+    console.log(
+      `\n🏃‍♂️ You ran ${distanceRan} km in your ${selectedShoe.brand} ${selectedShoe.modelName}.`
+    );
+    console.log(
+      `🔧 New durability left: ${
+        shoeData[selectedShoe.modelName].durabilityLeft
+      } km`
+    );
+    console.log(
+      `💥 Current wear level: ${visualizeWearLevel(
+        shoeData[selectedShoe.modelName].wearLevel
+      )}`
+    );
 
-      if (!shoeData[selectedShoe.modelName]) {
-        shoeData[selectedShoe.modelName] = {
-          totalDistance: 0,
-          durabilityLeft: selectedShoe.durabilityLeft,
-          wearLevel: 0,
-        };
-      }
+    PerformanceTracker.trackRun(shoeData, selectedShoe, distanceRan);
+    saveJSON("../shoeData.json", shoeData);
+    // update data
+    saveShoeData(shoeData);
 
-      shoeData[selectedShoe.modelName].totalDistance += distanceRan;
-      shoeData[selectedShoe.modelName].durabilityLeft -= distanceRan;
+    const recommendBasedOnWearLevel = await askQuestion(
+      "Would you like to recommend shoes based on wear level? (yes/no) "
+    );
+    if (recommendBasedOnWearLevel.toLowerCase() === "yes") {
+      console.log("\n📊 Recommending shoes based on wear level...");
+      let updatedRank = RecommendationEngine.recommend(
+        getActiveUserProfile(),
+        getActiveUserShoes()
+      ).filter((r) => r.shoe.durabilityLeft > 0);
 
-      const wearLevel = PerformanceTracker.calculateWearLevel(
-        shoeData[selectedShoe.modelName].totalDistance,
-        selectedShoe.durabilityLeft
+      // display updated recommendations
+      updatedRank.slice(0, 3).forEach((r, i) => {
+        console.log(`\n#${i + 1} 🥇 Score: ${r.score}`);
+        console.table(r.shoe);
+      });
+
+      // append the selected shoe to the shoe data
+      const newSelectedShoe = await askQuestion(
+        "Which shoe would you like to select and save? (1, 2, or 3) "
       );
-      shoeData[selectedShoe.modelName].wearLevel = wearLevel.toFixed(2);
-
-        
-      // updateed shoe status
-      console.log(
-        `\n🏃‍♂️ You ran ${distanceRan} km in your ${selectedShoe.brand} ${selectedShoe.modelName}.`
-      );
-      console.log(
-        `🔧 New durability left: ${
-          shoeData[selectedShoe.modelName].durabilityLeft
-        } km`
-      );
-      console.log(
-        `💥 Current wear level: ${visualizeWearLevel(
-          shoeData[selectedShoe.modelName].wearLevel
-        )}`
-      );
-
-      PerformanceTracker.trackRun(shoeData, selectedShoe, distanceRan);
-      saveJSON("../shoeData.json", shoeData);
-      // update data
-      saveShoeData(shoeData);
-
-
-      const recommendBasedOnWearLevel = await askQuestion(
-        "Would you like to recommend shoes based on wear level? (yes/no) "
-      );
-      if (recommendBasedOnWearLevel.toLowerCase() === "yes") {
-        console.log("\n📊 Recommending shoes based on wear level...");
-        let updatedRank = RecommendationEngine.recommend(
-          getActiveUserProfile(),
-          getActiveUserShoes()
-        ).filter((r) => r.shoe.durabilityLeft > 0);
-
-        // display updated recommendations
-        updatedRank.slice(0, 3).forEach((r, i) => {
-          console.log(`\n#${i + 1} 🥇 Score: ${r.score}`);
-          console.table(r.shoe);
-        });
-
-        // append the selected shoe to the shoe data
-        const newSelectedShoe = await askQuestion(
-          "Which shoe would you like to select and save? (1, 2, or 3) "
+      const index = parseInt(newSelectedShoe);
+      // save
+      if (!isNaN(index) && index >= 1 && index <= 3 && updatedRank[index - 1]) {
+        selectedShoe = updatedRank[index - 1].shoe;
+        saveJSON(`${activeUser().name}_selectedShoe.json`, selectedShoe);
+        console.log(
+          `\n👟 You selected: ${selectedShoe.brand} ${selectedShoe.modelName}`
         );
-        const index = parseInt(newSelectedShoe);
-        // save
-        if (!isNaN(index) && index >= 1 && index <= 3 && updatedRank[index - 1]) {
-          selectedShoe = updatedRank[index - 1].shoe;
-          saveJSON(`${activeUser().name}_selectedShoe.json`, selectedShoe);
-          console.log(
-            `\n👟 You selected: ${selectedShoe.brand} ${selectedShoe.modelName}`
-          );
-        } else {
-          console.log("❌ Invalid selection. No shoe saved.");
-        }
       } else {
-        console.log("No recommendations made.");
-      }
-
-      const trackAnotherRun = await askQuestion(
-        "Would you like to track another run? (yes/no) "
-      );
-      if (trackAnotherRun.toLowerCase() === "yes") {
-        console.log("Tracking another run...");
-
-        // run command action again
-        console.log("Running the run command again...");
-        runAction();
-      } else {
-        console.log("Exiting the run command...");
-        process.exit(0);
+        console.log("❌ Invalid selection. No shoe saved.");
       }
     } else {
-      console.log("❌ Invalid distance. Please enter a positive number.");
+      console.log("No recommendations made.");
     }
+
+    const trackAnotherRun = await askQuestion(
+      "Would you like to track another run? (yes/no) "
+    );
+    if (trackAnotherRun.toLowerCase() === "yes") {
+      console.log("Tracking another run...");
+
+      // run command action again
+      console.log("Running the run command again...");
+      runAction();
+    } else {
+      console.log("Exiting the run command...");
+      process.exit(0);
+    }
+  } else {
+    console.log("❌ Invalid distance. Please enter a positive number.");
   }
+}
