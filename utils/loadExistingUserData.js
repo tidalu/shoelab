ddaa0b766c7dce5a2821ea0f5ad25266a2d54698
Tidalu <127156318+tidalu@users.noreply.github.com>
2025-04-21 17:43:20 +0200

    extracted and created new module for LoadExistingFiles() function from app.js

---
+++ utils/loadExistingUserData.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+
+const  { askQuestion } = require( "./askQuestion.js");
+const  AthleteProfile = require( "../classes/AthleteProfile.js");
+const  { loadJSON} = require( "./FileManager.js");
+
+const dataPath = path.join(__dirname, '../data')
+
+
+async function LoadExistingFiles() {
+    const existingFiles = fs
+  .readdirSync(dataPath)
+  .filter((file) => file.endsWith("_profile.json"));
+  if (existingFiles.length === 0) return null;
+
+  console.log("\n📂 Existing profiles found:");
+
+  existingFiles.forEach((file, i) =>
+    console.log(`${i + 1}. ${file.replace("_profile.json", "")}`)
+  );
+
+  const index = await askQuestion(
+    "\n🔁 Load a profile? Enter number or press Enter to skip: "
+  );
+  if (!index) return null;
+
+  const name = existingFiles[parseInt(index) - 1].replace("_profile.json", "");
+  const profile = loadJSON(`${name}_profile.json`);
+  return new AthleteProfile(
+    profile.name,
+    profile.footSize,
+    profile.preferredTerrain,
+    profile.activityLevel,
+    profile.preferredType
+  );
+}
+
+
+module.exports = LoadExistingFiles;
