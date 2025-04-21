ec32881fd9fb9528186f7028f32ef6266a5ad79a
Tidalu <127156318+tidalu@users.noreply.github.com>
2025-04-21 23:13:48 +0200

    created ShoeTypes

---
+++ classes/ShoeTypes.js
@@ -0,0 +1,9 @@
+const HikingBoot = require('./HikingBoot');
+const RunningShoe = require('./RunningShoe');
+const ShoeTypes = {
+    RunningShoe: RunningShoe,
+    HikingBoot: HikingBoot,
+};
+
+
+module.exports = ShoeTypes;
