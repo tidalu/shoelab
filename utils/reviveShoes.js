2db94aaee5e4cfa1accc08f94ee74611fccdfae5
Tidalu <127156318+tidalu@users.noreply.github.com>
2025-05-19 11:48:21 +0200

    refactor: improve shoe revival logic

--- utils/reviveShoes.js
+++ utils/reviveShoes.js
@@ -1,20 +1,37 @@
 const { RunningShoe, HikingBoot } = require("../classes/ShoeTypes");
 
+// const shoeData = {
+//   type: "RunningShoe",
+//   brand: "Nike",
+//   modelName: "Air Max",
+//   size: 42,
+//   material: "Mesh",
+//   durabilityLeft: 100,
+//   cushioningLevel: "High",
+//   wearLevel: "10.0",
+// };  smple shoe data
+
 function reviveShoe(shoe) {
+  let revived;
   switch (shoe.type) {
     case "RunningShoe":
-      return Object.assign(new RunningShoe(), shoe);
+      revived = new RunningShoe();
+      break;
     case "HikingBoot":
-      return Object.assign(new HikingBoot(), shoe);
+      revived = new HikingBoot();
+      break;
     default:
-      console.warn(`Unknown shoe type: ${shoe.type}`);
+      console.warn(`Unknown shoe type`);
       return null;
   }
+  Object.assign(revived, shoe);
+  revived.wearLevel = parseFloat(shoe.wearLevel);
+  return revived;
 }
 
 function reviveShoes(shoeArray) {
   if (!Array.isArray(shoeArray)) {
-    return reviveShoe(shoeArray)
+    return reviveShoe(shoeArray);
   }
   return shoeArray.map(reviveShoe);
 }
