b523e2bd2b86602d09e1dc7bc0f98feba1f270e9
Tidalu <127156318+tidalu@users.noreply.github.com>
2025-05-19 12:08:48 +0200

    refactor: initialize wearLevel with 0 to prevent from being NaN

--- classes/RunningShoe.js
+++ classes/RunningShoe.js
@@ -1,63 +1,73 @@
 const Shoe = require("./Shoe.js");
 
-
 class RunningShoe extends Shoe {
-    constructor(brand, modelName, size, material, baseDurability = 100, cushioningLevel) {
-        super(brand, modelName, size, material, baseDurability);
-        this.cushioningLevel = cushioningLevel;
+  constructor(
+    brand,
+    modelName,
+    size,
+    material,
+    baseDurability = 100,
+    cushioningLevel
+  ) {
+    super(brand, modelName, size, material, baseDurability);
+    this.cushioningLevel = cushioningLevel;
+    this.wearLevel = 0;
+  }
+
+  simulateStep({ terrain = "track", intensity = 1 }) {
+    let wearMultiplier;
+
+    switch (terrain) {
+      case "road":
+        wearMultiplier = 1.2;
+        break;
+      case "trail":
+        wearMultiplier = 1.5;
+        break;
+      case "track":
+        wearMultiplier = 1.0;
+        break;
+      default:
+        wearMultiplier = 1.0;
+        break;
     }
 
+    const cushionFactor =
+      {
+        low: 1.1,
+        medium: 1.0,
+        high: 0.9,
+      }[this.cushioningLevel] || 1.0;
 
-    simulateStep({terrain = "track", intensity = 1}) {
-        let wearMultiplier ;
+    const wearAdded = intensity * wearMultiplier * cushionFactor * 2;
+    this.wearLevel += wearAdded;
+  }
 
-
-        switch (terrain ) {
-            case "road": 
-                wearMultiplier = 1.2;
-                break;
-            case "trail":
-                wearMultiplier = 1.5;
-                break;
-            case "track":
-                wearMultiplier = 1.0;
-                break;
-            default:
-                wearMultiplier = 1.0;
-                break;
-        }
-        
-        const cushionFactor = {
-            low: 1.1,
-            medium: 1.0,
-            high: 0.9
-        }[this.cushioningLevel] || 1.0;
-
-        const wearAdded = intensity * wearMultiplier * cushionFactor * 2;
-        this.wearLevel += wearAdded;    
+  getComfortScore() {
+    let cushioningBonus = 0;
+    switch (this.cushioningLevel) {
+      case "High":
+        cushioningBonus = 20;
+        break;
+      case "Medium":
+        cushioningBonus = 10;
+        break;
+      case "Low":
+        cushioningBonus = 0;
+        break;
     }
+    return this.getRemainingDurability() + cushioningBonus;
+  }
 
-    getComfortScore() {
-        let cushioningBonus = 0;
-        switch (this.cushioningLevel) {
-          case 'High': cushioningBonus = 20; break;
-          case 'Medium': cushioningBonus = 10; break;
-          case 'Low': cushioningBonus = 0; break;
-        }
-        return this.getRemainingDurability() + cushioningBonus;
-}
-
-    getDetailedInfo() {
-        return {
-            ...this.getBasicInfo(),
-            type: "RunningShoe",
-            cushioningLevel: this.cushioningLevel,
-            comfortScore: this.getComfortScore(),
-            wearLevel: this.wearLevel.toFixed(2),
-          };
-    }
+  getDetailedInfo() {
+    return {
+      ...this.getBasicInfo(),
+      type: "RunningShoe",
+      cushioningLevel: this.cushioningLevel,
+      comfortScore: this.getComfortScore(),
+      wearLevel: this.wearLevel.toFixed(2),
+    };
+  }
 }
 
 module.exports = RunningShoe;
-
-
